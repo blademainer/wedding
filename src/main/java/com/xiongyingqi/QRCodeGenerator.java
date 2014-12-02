b7fe99997c1027eeecbc05125f6d3b8180886b4b
blademainer <blademainer@gmail.com>
2014-12-02 17:24:22 +0800

    init

---
+++ src/main/java/com/xiongyingqi/QRCodeGenerator.java
@@ -0,0 +1,10 @@
+package com.xiongyingqi;
+
+/**
+ * Created by qi<a href="http://xiongyingqi.com">xiongyingqi.com</a> on 14-12-2.
+ */
+public class QRCodeGenerator {
+    public static void main(String[] args){
+
+    }
+}
